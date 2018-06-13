bc34c857e51215e18412c7cb6ebb8770a0284f5e
Christophe Pollet <cpollet@users.noreply.github.com>
2018-06-13 20:45:23 +0200

    removed useless comment

--- src/it/basic/postbuild.groovy
+++ src/it/basic/postbuild.groovy
@@ -1,5 +1,3 @@
-// import java.util.regex.Pattern
-
 File collectionFile = new File(basedir, "target/basic-version.json");
 
 assert collectionFile.isFile()
--- src/it/classpath/postbuild.groovy
+++ src/it/classpath/postbuild.groovy
@@ -1,5 +1,3 @@
-// import java.util.regex.Pattern
-
 File collectionFile = new File(basedir, "api/target/classpath-api-version.json");
 
 assert collectionFile.isFile()
