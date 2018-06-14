bd0dd43413d98b15638434a15d655f50cbf3a32f
Christophe Pollet <cpollet@users.noreply.github.com>
2018-06-14 23:13:21 +0200

    added missing tests

--- src/test/java/net/cpollet/maven/plugins/postman/backend/ClassScannerTest.java
+++ src/test/java/net/cpollet/maven/plugins/postman/backend/ClassScannerTest.java
@@ -28,6 +28,42 @@ public class ClassScannerTest {
                 .contains("net.cpollet.maven.plugins.postman.it.api.Api");
     }
 
+    @Test
+    public void find_doesNotReturnClassesOutsideOfPackage() {
+        // GIVEN
+        List<String> path = Collections.singletonList(resourcePath(API_JAR));
+        ClassScanner scanner = new ClassScanner(path, Collections.singletonList("com"));
+
+        // WHEN
+        List<Class> classes = scanner.find();
+
+        // THEN
+        List<String> classNames = classes.stream()
+                .map(Class::getCanonicalName)
+                .collect(Collectors.toList());
+
+        Assertions.assertThat(classNames)
+                .doesNotContain("net.cpollet.maven.plugins.postman.it.api.Api");
+    }
+
+    @Test
+    public void find_doesNotReturnClassesWithoutAnnotation() {
+        // GIVEN
+        List<String> path = Collections.singletonList(resourcePath(API_JAR));
+        ClassScanner scanner = new ClassScanner(path, Collections.emptyList());
+
+        // WHEN
+        List<Class> classes = scanner.find();
+
+        // THEN
+        List<String> classNames = classes.stream()
+                .map(Class::getCanonicalName)
+                .collect(Collectors.toList());
+
+        Assertions.assertThat(classNames)
+                .doesNotContain("net.cpollet.maven.plugins.postman.it.api.User");
+    }
+
     private String resourcePath(String resource) {
         return getClass().getResource(resource).getPath();
     }
